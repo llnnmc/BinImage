7973126674284dc19c5ba1f81ba9dcc35c4b5df1
liuln <liuln@yizit.cn>
2019-12-04 21:57:48 +0800

    version 1.0

---
+++ binImg/binImg/BinImg.cs
@@ -0,0 +1,128 @@
+﻿using System.Drawing;
+using System.Drawing.Imaging;
+
+namespace binImg
+{
+    public class BinImg
+    {
+        private Bitmap bmp;
+
+        // 构造图像对象
+        public BinImg(string imgFile)
+        {
+            bmp = new Bitmap(imgFile, true);
+
+        }
+
+        // 图像灰度化
+        public Bitmap ToGray()
+        {
+            for (int i = 0; i < bmp.Width; i++)
+            {
+                for (int j = 0; j < bmp.Height; j++)
+                {
+                    // 获取像素点颜色
+                    Color pixelColor = bmp.GetPixel(i, j);
+
+                    // 计算灰度值
+                    int gray = (int)(0.299 * pixelColor.R + 0.587 * pixelColor.G + 0.114 * pixelColor.B);
+                    Color newColor = Color.FromArgb(gray, gray, gray);
+                    bmp.SetPixel(i, j, newColor);
+                }
+            }
+            return bmp;
+        }
+
+        // 灰度图像二值化
+        public Bitmap Binaryzation()
+        {
+            int[] histogram = new int[256];
+            int minGrayValue = 255, maxGrayValue = 0;
+
+            // 求取直方图
+            for (int i = 0; i < bmp.Width; i++)
+            {
+                for (int j = 0; j < bmp.Height; j++)
+                {
+                    Color pixelColor = bmp.GetPixel(i, j);
+                    histogram[pixelColor.R]++;
+                    if (pixelColor.R > maxGrayValue) maxGrayValue = pixelColor.R;
+                    if (pixelColor.R < minGrayValue) minGrayValue = pixelColor.R;
+                }
+            }
+
+            // 迭代计算阀值
+            int threshold = -1;
+            int newThreshold = (minGrayValue + maxGrayValue) / 2;
+            for (int iterationTimes = 0; threshold != newThreshold && iterationTimes < 100; iterationTimes++)
+            {
+                threshold = newThreshold;
+                int lP1 = 0;
+                int lP2 = 0;
+                int lS1 = 0;
+                int lS2 = 0;
+
+                // 求两个区域的灰度的平均值
+                for (int i = minGrayValue; i < threshold; i++)
+                {
+                    lP1 += histogram[i] * i;
+                    lS1 += histogram[i];
+                }
+                int mean1GrayValue = (lP1 / lS1);
+                for (int i = threshold + 1; i < maxGrayValue; i++)
+                {
+                    lP2 += histogram[i] * i;
+                    lS2 += histogram[i];
+                }
+                int mean2GrayValue = (lP2 / lS2);
+                newThreshold = (mean1GrayValue + mean2GrayValue) / 2;
+            }
+
+            // 二值化
+            for (int i = 0; i < bmp.Width; i++)
+            {
+                for (int j = 0; j < bmp.Height; j++)
+                {
+                    Color pixelColor = bmp.GetPixel(i, j);
+                    if (pixelColor.R > threshold)
+                    {
+                        bmp.SetPixel(i, j, Color.FromArgb(255, 255, 255));
+                    }
+                    else
+                    {
+                        bmp.SetPixel(i, j, Color.FromArgb(0, 0, 0));
+                    }
+                }
+            }
+            return bmp;
+        }
+
+        // 二值化图像反转
+        public Bitmap Reverse()
+        {
+            for (int i = 0; i < bmp.Width; i++)
+            {
+                for (int j = 0; j < bmp.Height; j++)
+                {
+                    Color pixelColor = bmp.GetPixel(i, j);
+                    Color newColor = Color.FromArgb(255 - pixelColor.R, 255 - pixelColor.G, 255 - pixelColor.B);
+                    bmp.SetPixel(i, j, newColor);
+                }
+            }
+            return bmp;
+        }
+
+        // 图像裁切
+        public Bitmap Tailor(Rectangle rect)
+        {
+            bmp = bmp.Clone(rect, PixelFormat.DontCare);
+            return bmp;
+        }
+
+        // 保存图像文件
+        public void SaveImg(string imgFile)
+        {
+            bmp.Save(imgFile);
+        }
+    }
+}
---
+++ binTest/binTest/Program.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+// 增加对binImg类库的引用
+using binImg;
+
+namespace binTest
+{
+    class Program
+    {
+        public static void ShowTip()
+        {
+            Console.WriteLine("\r\n图像二值化测试程序V1.0，上海因致信息科技有限公司，2018/06。");
+            Console.WriteLine("\r\n命令行格式：");
+            Console.WriteLine("binTest <输入图像文件名> <输出图像文件名> <-[g][b][r][t] [x] [y] [width] [height]>");
+            Console.WriteLine("-g：灰度化处理");
+            Console.WriteLine("-b：二值化处理");
+            Console.WriteLine("-r：反转处理");
+            Console.WriteLine("-t：裁切处理");
+            Console.WriteLine("x，y，width，height：裁切起始位置坐标和宽高度");
+        }
+
+        static void Main(string[] args)
+        {
+            // 命令行检查和提示
+            if (args.Length != 3 && args.Length != 7)
+            {
+                ShowTip();
+                return;
+            }
+
+            if (args[2] == "-gbrt" && args.Length != 7)
+            {
+                ShowTip();
+                return;
+            }
+
+            try
+            {
+                // 构造二值化图像对象
+                BinImg bi = new BinImg(args[0]);
+                switch (args[2])
+                {
+                    case "-g":
+                        // 灰度化
+                        bi.ToGray();
+                        break;
+                    case "-gb":
+                        // 灰度化
+                        bi.ToGray();
+                        // 二值化
+                        bi.Binaryzation();
+                        break;
+                    case "-gbr":
+                        // 灰度化
+                        bi.ToGray();
+                        // 二值化
+                        bi.Binaryzation();
+                        // 反转
+                        bi.Reverse();
+                        break;
+                    case "-gbrt":
+                        // 灰度化
+                        bi.ToGray();
+                        // 二值化
+                        bi.Binaryzation();
+                        // 反转
+                        bi.Reverse();
+                        // 裁切
+                        Rectangle rect = new Rectangle(Int32.Parse(args[3]), Int32.Parse(args[4]), Int32.Parse(args[5]), Int32.Parse(args[6]));
+                        bi.Tailor(rect);
+                        break;
+                    case "-b":
+                        // 二值化
+                        bi.Binaryzation();
+                        break;
+                    case "-br":
+                        // 二值化
+                        bi.Binaryzation();
+                        // 反转
+                        bi.Reverse();
+                        break;
+                    case "-brt":
+                        // 二值化
+                        bi.Binaryzation();
+                        // 反转
+                        bi.Reverse();
+                        // 裁切
+                        rect = new Rectangle(Int32.Parse(args[3]), Int32.Parse(args[4]), Int32.Parse(args[5]), Int32.Parse(args[6]));
+                        bi.Tailor(rect);
+                        break;
+                    case "-r":
+                        // 反转
+                        bi.Reverse();
+                        break;
+                    case "-rt":
+                        // 反转
+                        bi.Reverse();
+                        // 裁切
+                        rect = new Rectangle(Int32.Parse(args[3]), Int32.Parse(args[4]), Int32.Parse(args[5]), Int32.Parse(args[6]));
+                        bi.Tailor(rect);
+                        break;
+                    case "-t":
+                        // 裁切
+                        rect = new Rectangle(Int32.Parse(args[3]), Int32.Parse(args[4]), Int32.Parse(args[5]), Int32.Parse(args[6]));
+                        bi.Tailor(rect);
+                        break;
+                    default:
+                        ShowTip();
+                        return;
+                }
+
+                //写出位图文件
+                bi.SaveImg(args[1]);
+
+                Console.WriteLine("\r\n图像二值化测试程序V1.0，上海因致信息科技有限公司，2018/06。");
+                Console.WriteLine("\r\nCompleted!");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+    }
+}
